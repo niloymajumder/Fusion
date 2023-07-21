7c5c138b29906f3f638ece07d47557f6166b7cc4
Joy Majumdar <gopalmajumdarjoy@gmail.com>
2023-07-21 18:49:30 +0600

    Mail-Server-Added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-function auth() {
-    window.open("./User/");
-}
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyZftqcbofl_Wmr6jPagevo-p75S8UZypcCYBB2xFSLV0yMhHsBeqsTnQvxFSRQTRhVHQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("Ara-Ara-msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Thanks For Subscribing";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
